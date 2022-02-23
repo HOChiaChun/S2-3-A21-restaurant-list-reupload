b0e00abfcf50b5f9af0f9f71c651827b26539e00
HO Chia Cung <john778300@gmail.com>
2022-02-23 08:12:08 +0800

    1.去掉多餘註解，並將key-value同名的部份使用Object shorthand 2.增加了搜尋時的關鍵字停留

--- app.js
+++ app.js
@@ -17,26 +17,14 @@ app.get("/", (req, res) => {
 
 app.get("/restaurants/:restaurant_id", (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render("show", { restaurant: restaurant })
+  res.render("show", { restaurant })
 })
 
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
-  
-  res.render("index", { restaurants: restaurants, keyword: keyword })
+  res.render("index", { restaurants, keyword })
 })
-
-/*也可以寫成非匿名函式*/
-/*app.get("/search", (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurantFilter)
-  function restaurantFilter (restaurant) {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  }
-  res.render("index", { restaurants: restaurants, keyword: keyword })
-})*/
-
 
 app.listen(port, () => {
   console.log(`This Web Server is on : http://localhost:${port}`)
