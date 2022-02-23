dbc6f54346a6fcc8db9973664aa19dcfb4dd17e1
HO Chia Cung <john778300@gmail.com>
2022-02-23 08:12:08 +0800

    feat: webserver link with database and add schema

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const app = express()
 const port = 3000
 const exphbs = require("express-handlebars")
 const restaurantList = require("./restaurant.json")
+const mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost/restaurant-list")
+
+const db = mongoose.connection
+
+db.on("rerror", () => {
+  console.log("mongodb error!")
+})
+
+db.once("open", () => {
+  console.log("mongodb connected!")
+})
+
+//我已做到這步，已讓webserver連線上 database
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
@@ -17,26 +31,14 @@ app.get("/", (req, res) => {
 
 app.get("/restaurants/:restaurant_id", (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render("show", { restaurant: restaurant })
+  res.render("show", { restaurant })
 })
 
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
-  
-  res.render("index", { restaurants: restaurants, keyword: keyword })
+  res.render("index", { restaurants, keyword })
 })
-
-/*也可以寫成非匿名函式*/
-/*app.get("/search", (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurantFilter)
-  function restaurantFilter (restaurant) {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  }
-  res.render("index", { restaurants: restaurants, keyword: keyword })
-})*/
-
 
 app.listen(port, () => {
   console.log(`This Web Server is on : http://localhost:${port}`)
