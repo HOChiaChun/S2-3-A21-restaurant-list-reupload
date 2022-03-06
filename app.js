5af5f138dbad99236dd272220ba9ee6755f79c24
HO Chia Cung <john778300@gmail.com>
2022-03-06 13:32:37 +0800

    feat: add front page routing and create new page routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
+const Restaurant = require("./models/restaurant")
 
 mongoose.connect("mongodb://localhost/restaurant-list")
 
@@ -16,8 +17,6 @@ db.once("open", () => {
   console.log("mongodb connected!")
 })
 
-//我已做到這步，已讓webserver連線上 database
-
 app.engine("handlebars", exphbs({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
 
@@ -25,7 +24,14 @@ app.use(express.static("public"))
 
 
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render("index", { restaurants }))
+    .catch(error => console.log(error))
+})
+
+app.get("/restaurants/new", (req, res) => {
+  return res.render("new")
 })
 
 app.get("/restaurants/:restaurant_id", (req, res) => {
