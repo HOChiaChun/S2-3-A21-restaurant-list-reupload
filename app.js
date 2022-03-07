df9794e6550e378e41f07c1662ebc89ec0872ed4
HO Chia Cung <john778300@gmail.com>
2022-03-07 08:46:27 +0800

    feat: add edit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
 const Restaurant = require("./models/restaurant")
 const bodyParser = require("body-parser")
+const restaurant = require("./models/restaurant")
 
 const app = express()
 const port = 3000
@@ -59,6 +60,42 @@ app.get("/restaurants/:restaurant_id", (req, res) => {
   return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render("show", { restaurant }))
+})
+
+app.get("/restaurants/:restaurant_id/edit", (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render("edit", { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post("/restaurants/:restaurant_id/edit", (req, res) => {
+  const id = req.params.restaurant_id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`)) 
+    .catch(error => console.log(error))
 })
 
 app.get("/search", (req, res) => {
