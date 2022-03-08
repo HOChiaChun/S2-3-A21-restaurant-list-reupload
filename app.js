b4a0ddefd97820928eaf7670c4deefcff785d8d1
HO Chia Cung <john778300@gmail.com>
2022-03-09 00:47:25 +0800

    refactory: modify routes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express") 
 const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
-const Restaurant = require("./models/restaurant")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override")
+const routes = require("./routes")
 
 const app = express()
 const port = 3000
@@ -27,94 +27,7 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride("_method"))
-
-
-app.get("/", (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render("index", { restaurants }))
-    .catch(error => console.log(error))
-})
-
-app.get("/restaurants/new", (req, res) => {
-  return res.render("new")
-})
-
-app.post("/restaurants", (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create({
-    name, name_en, category, image, location, phone, google_map, rating, description})
-    .then(() => res.redirect("/"))
-    .catch(error => console.log(error))
-})
-
-app.get("/restaurants/:restaurant_id", (req, res) => {
-  const id = req.params.restaurant_id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render("show", { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.get("/restaurants/:restaurant_id/edit", (req, res) => {
-  const id = req.params.restaurant_id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render("edit", { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.put("/restaurants/:restaurant_id", (req, res) => {
-  const id = req.params.restaurant_id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`)) 
-    .catch(error => console.log(error))
-})
-
-app.delete("/restaurants/:restaurant_id", (req, res) => {
-  const id = req.params.restaurant_id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect("/"))
-    .catch(error => console.log(error))
-})
-
-app.get("/search", (req, res) => {
-  const keyword = req.query.keyword.trim()
-  Restaurant.find({ name: { $regex: keyword, $options: "i" } })
-    .lean()
-    .then(restaurants => res.render("index", { restaurants, keyword }))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 
 
