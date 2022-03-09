341ce9f14972a6401af8c7f07ba854e1c5b5a6f7
HO Chia Cung <john778300@gmail.com>
2022-03-09 22:38:51 +0800

    feat: add sort function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,11 +4,31 @@ const router = express.Router()
 const Restaurant = require("../../models/restaurant")
 
 router.get("/search", (req, res) => {
+  
+  const sort = req.query.sort 
   const keyword = req.query.keyword.trim()
-  Restaurant.find({ name: { $regex: keyword, $options: "i" } })
-    .lean()
-    .then(restaurants => res.render("index", { restaurants, keyword }))
-    .catch(error => console.log(error))
+
+  switch (sort) {
+    case "A > Z":
+      mode = 'asc'
+      break;
+    case "Z > A":
+      mode = 'desc'
+      break;
+  }
+
+  if (!req.query.keyword) {
+    Restaurant.find()
+      .lean()
+      .sort({ name: mode })
+      .then(restaurants => res.render("index", { restaurants, sort }))
+      .catch(error => console.log(error))
+  } else {
+    Restaurant.find({ name: { $regex: keyword, $options: "i" } })
+      .lean()
+      .then(restaurants => res.render("index", { restaurants, keyword }))
+      .catch(error => console.log(error))
+  }
 })
 
 
