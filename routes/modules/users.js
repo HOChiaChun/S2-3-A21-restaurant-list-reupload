18646021fa367154025b8860d54950ecbe9c0ada
HO Chia Cung <john778300@gmail.com>
2022-04-02 10:10:24 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -20,6 +20,11 @@ router.post("/register", (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 
 
 module.exports = router
