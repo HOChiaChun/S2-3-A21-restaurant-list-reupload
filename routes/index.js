7bb6e21c9d2fd9e5e434bf56cfe87cda564a4658
HO Chia Cung <john778300@gmail.com>
2022-04-02 14:42:38 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const home = require("./modules/home")
 const restaurants = require("./modules/restaurants")
 const search = require("./modules/search")
 const users = require("./modules/users")
+const auth = require("./modules/auth")
 const { authenticator } = require("../middleware/auth")
 
 
 router.use("/restaurants",  authenticator, restaurants)
 router.use("/users", users)
+router.use("/auth", auth)
 router.use("/", search)
 router.use("/", authenticator, home)
 
