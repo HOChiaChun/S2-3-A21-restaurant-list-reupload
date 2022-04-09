b69ec36ce4b4288cf8888e9a10a63c37801d8735
HO Chia Cung <john778300@gmail.com>
2022-04-09 23:53:48 +0800

    refactory: modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,10 +1,14 @@
 const bcrypt = require("bcryptjs")
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+
 const Restaurant = require("../restaurant")
 const User = require('../user')
-const restaurantList = require("../../restaurant.json")
+const restaurantUser1List = require("../../restaurantUser1.json")
+const restaurantUser2List = require("../../restaurantUser2.json")
 const db = require("../../config/mongoose")
 
 const SEED_USER = [
@@ -12,62 +16,52 @@ const SEED_USER = [
   { name: 'User2', email: 'user2@example.com', password: '12345678'}
  ]
 
-db.once("open", () => {
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER[0].password, salt))
-    .then(hash => User.create({
-      name: SEED_USER[0].name,
-      email: SEED_USER[0].email,
-      password: hash
-    }))
-    .then(user => {
-      const userId = user._id
-      return Promise.all(Array.from(
-        { length: 3 },
-        (_, i) => Restaurant.create({
-          name: restaurantList.results[i].name,
-          name_en: restaurantList.results[i].name_en,
-          category: restaurantList.results[i].category,
-          image: restaurantList.results[i].image,
-          location: restaurantList.results[i].location,
-          phone: restaurantList.results[i].phone,
-          google_map: restaurantList.results[i].google_map,
-          rating: restaurantList.results[i].rating,
-          description: restaurantList.results[i].description,
-          userId
-        })
-      ))
-    })
-    .then(() => {
+
+
+
+
+db.once('open', () => {
+  Promise.all([
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER[0].password, salt))
+      .then(hash => User.create({
+        name: SEED_USER[0].name,
+        email: SEED_USER[0].email,
+        password: hash
+      }))
+      .then(user => {
+        return Promise.all(Array.from(
+          { length: 3 },
+          (_, i) => {
+            const userId = user._id
+            const { name, name_en, category, image, location, phone, google_map, rating, description } = restaurantUser1List.results[i]
+            return Restaurant.create({ userId, name, name_en, category, image, location, phone, google_map, rating, description })
+          }
+        ))
+      })
+    , 
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER[1].password, salt))
+      .then(hash => User.create({
+        name: SEED_USER[1].name,
+        email: SEED_USER[1].email,
+        password: hash
+      }))
+      .then(user => {
+        return Promise.all(Array.from(
+          { length: 3 },
+          (_, i) => {
+            const userId = user._id
+            const { name, name_en, category, image, location, phone, google_map, rating, description } = restaurantUser2List.results[i]
+            return Restaurant.create({ userId, name, name_en, category, image, location, phone, google_map, rating, description })
+          }
+        ))
+      })
+  ])
+  .then(() => {
+      console.log('done.')
       process.exit()
-    })
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER[1].password, salt))
-    .then(hash => User.create({
-      name: SEED_USER[1].name,
-      email: SEED_USER[1].email,
-      password: hash
-    }))
-    .then(user => {
-      const userId = user._id
-      for(let i = 3; i <= 5; i++) {
-        Restaurant.create({
-          name: restaurantList.results[i].name,
-          name_en: restaurantList.results[i].name_en,
-          category: restaurantList.results[i].category,
-          image: restaurantList.results[i].image,
-          location: restaurantList.results[i].location,
-          phone: restaurantList.results[i].phone,
-          google_map: restaurantList.results[i].google_map,
-          rating: restaurantList.results[i].rating,
-          description: restaurantList.results[i].description,
-          userId
-        })
-      }
-    })
-    .then(() => {
-      console.log("done")
-    })
+  })
 })
